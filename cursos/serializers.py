8accb9e2742af0b9c6aac82824f4db0f947647a5
Kevin Soffa <kevinsoffa@gmail.com>
2021-11-09 11:34:13 -0300

    Terceiro deploy
    Limitando o número de requisições com Throttling
    ----------------------------------------------------
    Customizando a validação de dados - Nível de campo
    -(Função "validate") -> Serializers
    ----------------------------------------------------
    Customizando a serialização dos dados
    -(media de notas) -> SerializerMethodField
    -Classe Avg

--- cursos/serializers.py
+++ cursos/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from django.db.models import Avg
 
 from .models import Curso, Avaliacao
 
@@ -21,6 +22,12 @@ class AvaliacaoSerializer(serializers.ModelSerializer):
             'ativo'
         )
 
+        def validate_avaliacao(self, valor):
+            if valor in range(1, 6):  # 1, 2, 3, 4, 5
+                return valor
+            raise serializers.ValidationError(
+                'As notas de AVALIAÇÃO devem ser um número inteiro entre 1 a 5.')
+
 
 class CursoSerializer(serializers.ModelSerializer):
     # Nested Realationship
@@ -37,6 +44,7 @@ class CursoSerializer(serializers.ModelSerializer):
     # Primary Key Related Field -> Forma mais performatica
     avaliacoes = serializers.PrimaryKeyRelatedField(many=True,
                                                     read_only=True)
+    media_avaliacoes = serializers.SerializerMethodField()
 
     class Meta:
         model = Curso
@@ -46,5 +54,13 @@ class CursoSerializer(serializers.ModelSerializer):
             'url',
             'criacao',
             'ativo',
-            'avaliacoes'
+            'avaliacoes',
+            'media_avaliacoes'
         )
+
+    def get_media_avaliacoes(self, obj):
+        media = obj.avaliacoes.aggregate(Avg('avaliacao')).get('avaliacao__avg')
+
+        if media is None:
+            return 0
+        return round(media * 2) / 2
--- escola/settings.py
+++ escola/settings.py
@@ -143,5 +143,13 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticatedOrReadOnly',
     ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 2
+    'PAGE_SIZE': 2,
+    'DEFAULT_THROTTLE_CLASSES': (
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle',
+    ),
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '5/minute',  # second, day, month, year
+        'user': '10/minute'
+    }
 }
